584a92e6610463edc9d45e71ce9e2315b8237063
Michael Peels <michaelpeels@Michael-Peels.local>
2024-10-31 18:47:34 -0400

    Init

---
+++ api/src/main/java/com/thj/charviewer/CharviewerApplication.java
@@ -0,0 +1,13 @@
+package com.thj.charviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharviewerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CharviewerApplication.class, args);
+  }
+
+}
---
+++ api/src/test/java/com/thj/charviewer/CharviewerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.thj.charviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharviewerApplicationTests {
+
+  @Test
+  void contextLoads() {
+    // context loads
+  }
+
+}
