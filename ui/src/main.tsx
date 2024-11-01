de3e222b1c34494548dbecd563e488e32fc13dfe
Michael Peels <michaelpeels@Michael-Peels.local>
2024-10-31 23:24:35 -0400

    Testing search

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Home } from "./home/Home";
+import "./main.scss";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  </StrictMode>
+);
