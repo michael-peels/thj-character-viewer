2c8212016244bb87ef4b5aff397183fa6c0f5f8c
Michael Peels <michaelpeels@Michael-Peels.local>
2024-11-05 18:30:51 -0500

    Move param management to hook

--- ui/src/character-search/CharacterSearch.tsx
+++ ui/src/character-search/CharacterSearch.tsx
@@ -1,48 +1,27 @@
-import { FormEvent, useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { CharacterSearchResults } from '../generated';
+import { FormEvent, useState } from 'react';
 import { useCharacterSearch } from '../hooks/api/useCharacterSearch';
 import { CharacterResultList } from './CharacterResultList';
 import styles from './character-search.module.scss';
 
 export const Home = () => {
-    const { search } = useCharacterSearch();
+    const { search, page, results, name } = useCharacterSearch();
     const [searchText, setSearchText] = useState('');
-    const [searchResults, setSearchResults] = useState<CharacterSearchResults | undefined>();
-    const [searchParams, setSearchParams] = useSearchParams();
-
-    useEffect(() => {
-        const name = searchParams.get('name');
-        if (name) {
-            const page = Number(searchParams.get('page'));
-            search(name, page).then((result) => setSearchResults(result));
-        }
-    }, [searchParams]);
 
     const handleSearch = async (event: FormEvent) => {
         event.preventDefault();
         if (searchText.trim() !== '') {
-            setSearchParams({ name: searchText });
+            search(searchText);
         }
-    };
-
-    const handlePageChange = (newPage: number) => {
-        const allValues = searchParams.toString();
-        console.log('searchParams', allValues);
-        setSearchParams({ name: searchParams.get('name') ?? '', page: newPage.toString() });
     };
 
     return (
         <div className={styles.home}>
             <img className={styles.logo} src="thjlogo.png" alt="The Heroe's Journey logo" />
             <form className={styles.search} onSubmit={handleSearch}>
-                <input
-                    onChange={(event) => setSearchText(event.target.value)}
-                    defaultValue={searchParams.get('name') ?? ''}
-                />
+                <input onChange={(event) => setSearchText(event.target.value)} defaultValue={name} />
                 <button>Search</button>
             </form>
-            {searchResults && <CharacterResultList results={searchResults} onChangePage={handlePageChange} />}
+            {results && <CharacterResultList results={results} onChangePage={page} />}
         </div>
     );
 };
--- ui/src/hooks/api/useCharacterSearch.ts
+++ ui/src/hooks/api/useCharacterSearch.ts
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
 import { CharacterSearchResults, SearchControllerService } from '../../generated';
+import { useSearchParams } from 'react-router-dom';
 
 const PAGE_SIZE = 25;
 export const useCharacterSearch = () => {
-    const search = async (searchString: string, page: number = 0): Promise<CharacterSearchResults> => {
-        return await SearchControllerService.findCharacters({ name: searchString, page, size: PAGE_SIZE });
+    const [results, setResults] = useState<CharacterSearchResults | undefined>();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    useEffect(() => {
+        const name = searchParams.get('name');
+        const page = Number(searchParams.get('page'));
+        if (name && name.trim() !== '') {
+            performSearch(name, page);
+        }
+    }, [searchParams]);
+
+    const performSearch = async (searchString: string, page: number = 0) => {
+        setResults(await SearchControllerService.findCharacters({ name: searchString, page, size: PAGE_SIZE }));
     };
 
-    return { search };
+    const search = async (searchString: string, page: number = 0) => {
+        setSearchParams({ name: searchString, page: page.toString() });
+    };
+
+    const page = (page: number) => {
+        search(searchParams.get('name') ?? '', page);
+    };
+
+    return { search, page, results, name: searchParams.get('name') ?? '' };
 };
